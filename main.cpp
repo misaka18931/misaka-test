cf21ebf970d20bddaf965fc58b91f11a5fd42aa7
misaka18931-desktop <misaka18931@protonmail.com>
2022-05-25 14:32:24 +0800

    sample in posix shell

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <unistd.h>
+#include <signal.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <cstdio>
+#include <cstring>
+
+static void handler(int signum) {
+  fprintf(stderr, "timer expired\n");
+  kill(0, SIGTERM);
+}
+
+enum verdict {
+  accepted,  wrong_answer, time_limit, memory_limit, runtime_error
+};
+
+
+
+int main(int argc, char **argv) {
+  pid_t pid = fork();
+  if (pid == -1) {
+    fprintf(stderr, "fork failure.\n");
+    return 1;
+  } else if (pid == 0) {
+    setsid();
+    // timer
+    itimerval virt_timer = {{0l, 0l}, {1l, 500000l}}; // 2500ms, one shot
+    if (setitimer(ITIMER_REAL, &virt_timer, nullptr) == -1) {
+      fprintf(stderr, "timer setup failure.\n");
+      return 1;
+    }
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = handler;
+    sigaction(SIGALRM, &sa,nullptr);
+    pid_t se = fork();
+    if (pid == -1) {
+      fprintf(stderr, "second fork failure.\n");
+      return 1;
+    } else if (se == 0) {
+      rlimit mem_lim = {30000000,30000000};
+      setrlimit(RLIMIT_AS, &mem_lim);
+      execv(" 
+      test", argv);
+    }
+    int status;
+    rusage usage;
+    wait3(&status, 0, &usage);
+    printf("%d %d.%06d\n", usage.ru_maxrss, usage.ru_utime.tv_sec, usage.ru_utime.tv_usec);
+  }
+  wait(nullptr);
+  return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,71 @@
+/** @file
+ * @ingroup
+
+
+ */
+#include <bits/stdc++.h>
+using namespace std;
+template <typename T> inline void O(const T &x) { cout << x << '\n'; }
+template <typename T, typename... W> inline void O(const T &x, const W &...b) {
+  cout << x << ' ';
+  O(b...);
+}
+template <typename T> inline void rd(T &x) { cin >> x; }
+template <typename T, typename... W> inline void rd(T &x, W &...b) {
+  cin >> x;
+  rd(b...);
+}
+#ifndef MISAKA
+#define err(...)
+#else
+#define err(...) fprintf(stderr, __VA_ARGS__)
+#endif
+typedef long long i64;
+typedef unsigned long long u64;
+typedef unsigned u32;
+typedef long double dbl;
+typedef pair<int, int> pii;
+typedef uniform_int_distribution<int> r32;
+typedef uniform_int_distribution<i64> r64;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define shuf(L, R) shuffle((L), (R), rng)
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define FOR(i, j, k) for (int i = (j); i <= (k); ++i)
+#define ROF(i, j, k) for (int i = (k); i >= (j); --i)
+template <typename T> inline void ckmin(T &a, const T &b) { a = min(a, b); }
+template <typename T> inline void ckmax(T &a, const T &b) { a = max(a, b); }
+//#define IOFILE "filename"
+//#define MULTI
+const int N = 0;
+
+inline void sol() {
+  int x;
+  vector<int> g(10000000);
+  FOR(i,1,1000000) {
+    ++x;
+  }
+}
+
+int main() {
+  fprintf(stderr, "test called\n");
+#ifndef MISAKA
+#ifdef IOFILE
+  freopen(IOFILE ".in", "r", stdin);
+  freopen(IOFILE ".out", "w", stdout);
+#endif
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+#endif
+#ifdef MULTI
+  int T;
+  cin >> T;
+  while (T--)
+#endif
+    sol();
+  fprintf(stderr, "test normal\n");
+  return 0;
+}
+
